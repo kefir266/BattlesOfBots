5ff38436bfa2fe63955eb8c5208ba6dee5656d29
Dmitriy Gorbatko <kefir266@yahoo.com>
2017-01-10 21:55:07 +0300

    It was mademultitask for first step

--- GameAmazons/main.go
+++ GameAmazons/main.go
@@ -9,9 +9,9 @@ import (
 //	return((a) < 0 ? -(a) : (a))
 //}
 var N = 10
-
-var onlySpace bool
-
+var ch chan int
+var starttime int64
+var counter int
 type Dir struct {
 	y,x int
 }
@@ -24,6 +24,9 @@ type Field struct {
 	firstMove *Move
 	bestMove  *Move
 	lastMove  *Move
+	amazons[2][4] struct {
+		x,y int
+	}
 }
 
 type Move struct {
@@ -32,6 +35,7 @@ type Move struct {
 	rx, ry   int
 	cost     int
 	nextLink *Move
+	nAmazon  int
 }
 
 func (move *Move) add( nextLink *Move) {
@@ -48,9 +52,15 @@ func (field *Field) init() {
 func (field *Field) scan() error {
 
 	var err error = nil
+	var i[2] int
 	for y := 0 ; y < N ; y++ {
 		for x := 0 ; x < N ; x++ {
 			_, err = fmt.Scan(&field.mas[y][x])
+			if field.mas[y][x] == 1 || field.mas[y][x] == 2 {
+				field.amazons[field.mas[y][x] - 1][ i[field.mas[y][x] - 1] ].y = y
+				field.amazons[field.mas[y][x] - 1][ i[field.mas[y][x] - 1] ].x = x
+				i[field.mas[y][x]-1]++
+			}
 		}
 	}
 	_, err = fmt.Scan(&field.myPlayer)
@@ -58,17 +68,17 @@ func (field *Field) scan() error {
 	return err
 }
 
-func (field *Field) findAmazonArrow(py,px,y,x,player int) {
+func (field *Field) findAmazonArrow(py, px, y, x, player, nAmazon int) {
 	var cx, cy int
 	for _,d := range dir {
 		cx = x + d.x
 		cy = y + d.y
 		for ; (cx < N && cy < N && cx >= 0 && cy >= 0 && field.mas[cy][cx] == 0) ; {
 			if field.firstMove == nil {
-				field.firstMove = &Move{x: px, y: py, ax: x, ay: y, rx: cx, ry: cy}
+				field.firstMove = &Move{x: px, y: py, ax: x, ay: y, rx: cx, ry: cy, nAmazon: nAmazon}
 				field.lastMove = field.firstMove
 			} else {
-				field.lastMove.nextLink = &Move{x: px, y: py, ax: x, ay: y, rx: cx, ry: cy}
+				field.lastMove.nextLink = &Move{x: px, y: py, ax: x, ay: y, rx: cx, ry: cy, nAmazon: nAmazon}
 				field.lastMove = field.lastMove.nextLink
 			}
 
@@ -79,7 +89,7 @@ func (field *Field) findAmazonArrow(py,px,y,x,player int) {
 
 }
 
-func (field *Field) findAmazonMoves(y, x, player int) int {
+func (field *Field) findAmazonMoves(y, x, player, nAmazon int) int {
 	var num int = 0
 	field.mas[y][x] = 0
 
@@ -89,7 +99,8 @@ func (field *Field) findAmazonMoves(y, x, player int) int {
 		cy = y + d.y
 		for ; (cx < N && cy < N && cx >= 0 && cy >= 0 && field.mas[cy][cx] == 0) ; {
 
-			field.findAmazonArrow(y, x, cy, cx, player)
+			num++
+			field.findAmazonArrow(y, x, cy, cx, player, nAmazon)
 			cy += d.y
 			cx += d.x
 		}
@@ -98,15 +109,114 @@ func (field *Field) findAmazonMoves(y, x, player int) int {
 	return num
 }
 
-func (field *Field) calcMoves(player int) int {
-	var num int = 0
-	for  y := 0 ; y < N ; y++ {
-		for x := 0 ; x < N ; x++ {
-			if field.mas[y][x] == player {
-				num += field.findAmazonMoves(y, x, player)
-			}
+func (field *Field) copy() Field  {
+	var newField Field = Field{}
+	var y,x int
+
+	newField.init()
+
+	for y = 0 ; y < N ; y++ {
+		for x = 0 ;x < N ; x++ {
+			newField.mas[y][x] = field.mas[y][x]
 		}
 	}
+	var i = 0
+	for ; i < 4 ; i++ {
+		newField.amazons[0][i].y = field.amazons[0][i].y
+		newField.amazons[0][i].x = field.amazons[0][i].x
+	}
+	return newField
+}
+
+func (field *Field) nextStep(recur, player int, move *Move) int  {
+
+	field.mas[move.ay][move.ax] = 0
+	field.mas[move.y][move.x] = player
+	field.mas[move.ry][move.rx] = -1
+
+	field.amazons[player - 1][move.nAmazon].y = move.y
+	field.amazons[player - 1][move.nAmazon].x = move.x
+
+	//field.nextMove(recur,player)
+	move.cost = field.calcMoves(recur,player)
+
+	field.amazons[player - 1][move.nAmazon].y = move.ay
+	field.amazons[player - 1][move.nAmazon].x = move.ax
+
+	field.mas[move.ay][move.ax] = player
+	field.mas[move.y][move.x] = 0
+	field.mas[move.ry][move.rx] = 0
+
+	return move.cost
+}
+
+func (field *Field) passMoves(recur, player int, ch chan int) int {
+
+	counter++
+	if (recur > 2 || (counter > 10000000)) {
+		if recur == 1 {
+			ch <- 0
+			close(ch)
+		}
+		return 0
+	}
+
+	var currentMove *Move = field.firstMove
+	var nextField Field
+	nextField = field.copy()
+	nextField.myPlayer = player
+
+	var currentCost, bestCost int = 0,-10000
+
+	for ; currentMove != nil ; {
+
+		currentCost = nextField.nextStep(recur, player, currentMove)
+		if currentCost > bestCost {
+			bestCost = currentCost
+		}
+		currentMove = currentMove.nextLink
+	}
+	if recur == 1 {
+		ch <- bestCost
+		close(ch)
+	}
+
+
+	return bestCost
+}
+
+func (field *Field) calcMoves(recur,player int) int {
+	var num int = 0
+
+	var chn int = 0
+	var i int = 0;
+	for ; i < 4 ; i++ {
+
+		num += field.findAmazonMoves(field.amazons[player - 1][i].y, field.amazons[player - 1][i].x, player, i)
+				if recur == 0 {
+					chn++
+					go field.passMoves(recur+1, 3 - player, ch)
+				} else {
+					num -= field.passMoves(recur+1, 3 - player, nil)
+
+				}
+
+
+	}
+
+	if recur == 0 {
+		for i := range ch {
+			num -= i;
+			//chn--
+			if (chn == 0) {
+				break
+			}
+		}
+	} else if recur == 1 {
+//		ch <- num
+
+	}
+
 	return num
 }
 
@@ -126,7 +236,7 @@ func (field *Field) getBestMove() *Move{
 
 func (field *Field) nextMove(recur, player int) *Move{
 
-	field.calcMoves(player)
+	field.calcMoves(recur, player)
 	field.bestMove = field.getBestMove()
 	return field.bestMove
 }
@@ -138,10 +248,13 @@ var field = Field{}
 	{0,-1},{0,1},
 	{1,-1},{1,0},{1,1}}
 
+	ch = make(chan int, (N * N))
+
 	field.init()
 	field.scan()
 
-	starttime := time.Now().UnixNano();
+	starttime = time.Now().UnixNano();
+	counter = 0
 
 
 	field.nextMove(0,field.myPlayer)
@@ -153,4 +266,6 @@ var field = Field{}
 	fmt.Println(field.bestMove.ry, field.bestMove.rx)
 
 	fmt.Println((endtime - starttime))
+	fmt.Println(field.bestMove.cost)
+	fmt.Println(counter)
 }
